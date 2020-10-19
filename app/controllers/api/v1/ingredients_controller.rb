08fd55707da91881707df2ad60dff3b7354d33af
“Mineliegoma” <garlandegoma@gmail.com>
2020-10-19 16:36:19 -0400

    controllers_ingredients

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::IngredientsController < ApplicationController
-def index
- ingredient = Ingredient.all
- render json: ingredient
-end
+     def index
+      ingredient = Ingredient.all
+      render json: ingredient
+     end
+     def show
+         ingredient= Ingredient.find(params[:id])
+         render json: Ingredient
+       end 
+  
+
+  
+
+
+      private 
+        def ingredients_params
+         params.require(:ingredient).permit(:epices, :recipe_id)
+
+        end
+
 
 end
--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,8 +3,23 @@ class Api::V1::RecipesController < ApplicationController
  recipe = Recipe.all
  render json: recipe
 end
+def show
+        recipe = Recipe.find_by(id: params[:id])
+        if recipe
+            render json: recipe.to_json(:include => {
+                :ingredients => {:except => [:created_at, :updated_at]}
+            }, :except => [:created_at,:updated_at])
+        else
+            render json: { message: 'recipe not found' }
+        end
+    end
 
 
+private 
+  def recipe_params
+   params.require(:recipe).permit(:title, :image, :readyInMinutes)
+
+  end
 
 
 
